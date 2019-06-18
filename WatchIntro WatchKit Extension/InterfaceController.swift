88a0b15f82c3242cc814d482eccf7b52943128ce
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-06-18 12:24:06 -0400

    sent message from phone to watch

--- WatchIntro WatchKit Extension/InterfaceController.swift
+++ WatchIntro WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,29 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+        
+        
+    }
+    
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        
+        // OPEN THE MESSAGE
+        print("NAME: \(String(describing: message["name"])))")
+        self.lblOutput.setText("\(String(describing: message["name"]!))")
+        
+    }
+    
 
+    @IBOutlet weak var lblOutput: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,11 +40,37 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        print("Watch app is loaded!")
+        
+        // check if watch supports WCSession
+        if (WCSession.isSupported()) {
+            print("WATCH: Yes it supports session!")
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        } else {
+            print("WATCH : No, it doesnt support session")
+        }
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    var flipMsgs = true
 
+    @IBAction func btnPressed() {
+        print("btn pressed")
+        lblOutput.setText("heloooo hnziiii")
+        
+        if flipMsgs == true {
+            lblOutput.setText("HELLLOOOO")
+            flipMsgs = false
+        } else if flipMsgs == false {
+            lblOutput.setText("GOODBYE")
+            flipMsgs = true
+        }
+    }
 }
--- WatchIntro/ViewController.swift
+++ WatchIntro/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
+    
+    //MARK:- WCSessionDelegate functions
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+        
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    
+    
 
+    @IBOutlet weak var lblOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        // check if phone supports WCSession
+        if WCSession.isSupported() {
+            print("Yes it is")
+            
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        } else {
+            print("PHONE: Phone doesnt support session")
+        }
+        
     }
 
+    @IBAction func btnPressed(_ sender: Any) {
+        
+        print("Hello you pushed the btn")
+        lblOutput.text = "hnziii"
+        
+        // check if the watch is paired / accessible
+        if (WCSession.default.isReachable) {
+            // construct the message you want to send
+            // the message is in dictionary
+            let message = ["name": "Sagar"]
+            // send the message to the watch
+            WCSession.default.sendMessage(message, replyHandler: nil)
+            
+            print("Phone : Sent the data")
+            self.lblOutput.text = "Message sent"
+        } else {
+            print("Cannot find the watch")
+            self.lblOutput.text = "Error: Cannot find watch"
+        }
 
+        
+        
+    }
+    
 }
 

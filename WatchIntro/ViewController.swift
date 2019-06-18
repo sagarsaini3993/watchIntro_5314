4eb8eb8f55c531886f85e06c21057f7e2f506b7e
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-06-18 10:42:38 -0400

    Initial Commit

---
+++ WatchIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchIntro
+//
+//  Created by MacStudent on 2019-06-18.
+//  Copyright © 2019 sagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
